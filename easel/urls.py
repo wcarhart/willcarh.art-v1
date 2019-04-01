327f5bf327804ad6bd0c063581d295586468ca87
Will Carhart <wcarhart@sandiego.edu>
2019-03-31 22:01:27 -0700

    Added homepage

--- easel/urls.py
+++ easel/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('projects/', include('projects.urls')),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from projects.models import Project
 
-# Create your views here.
 def project_index(request):
 	projects = Project.objects.all()
 	context = {
