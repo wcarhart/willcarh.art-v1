3eca806053c9a31d2f002de7860d092c28b1c78c
Will Carhart <wcarhart@sandiego.edu>
2019-04-11 19:14:05 -0700

    Working on content now

--- projects/models.py
+++ projects/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-	title = models.CharField(max_length=100)
-	description = models.TextField()
-	technology = models.CharField(max_length=100)
-	image = models.FilePathField(path='/img')
+	title = models.CharField(max_length=100, default='', null=True, blank=True)
+	blurb = models.CharField(max_length=100, default='', null=True, blank=True)
+	description = models.TextField(default='', null=True, blank=True)
+	technology = models.TextField(default='', null=True, blank=True)
+	links = models.TextField(default='', null=True, blank=True)
+	cover = models.FilePathField(path='/img', default='', null=True, blank=True)
+	demo = models.FilePathField(path='/img', default='', null=True, blank=True)
 
 	def __str__(self):
 		return str(self.title)
