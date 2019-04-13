7c4c9829d7f1598da9db53b6f87368af906534b0
Will Carhart <wcarhart@sandiego.edu>
2019-04-13 16:38:41 -0700

    Added chronicler

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from projects.models import Project
+import json
 
 def project_index(request):
 	projects = Project.objects.all()
@@ -10,9 +11,19 @@ def project_index(request):
 
 def project_detail(request, id):
 	project = Project.objects.get(pk=id)
+
 	description_sections = project.description.split('<br>')
+	links_dictionary = json.loads(project.links.replace("'", '"'))
+	github = pypi = ''
+	if 'GitHub' in links_dictionary.keys():
+		github = links_dictionary['GitHub']
+	if 'PyPI' in  links_dictionary.keys():
+		pypi = links_dictionary['PyPI']
+
 	context = {
 		'project': project,
-		'description_sections': description_sections
+		'description_sections': description_sections,
+		'github': github,
+		'pypi': pypi
 	}
 	return render(request, 'project_detail.html', context)
