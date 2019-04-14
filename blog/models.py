6edcf879f600d652f0e3a2703106ee6b25231c9d
Will Carhart <wcarhart@sandiego.edu>
2019-04-14 11:03:42 -0700

    Created blog table in schema

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	article_number = models.IntegerField(default=0, blank=True, null=True)
+	title = models.CharField(max_length=100, default='', null=True, blank=True)
+	subtitle = models.CharField(max_length=100, default='', null=True, blank=True)
+	cover = models.FilePathField(path='/img', default='', null=True, blank=True)
+	content = models.TextField(default='', null=True, blank=True)
+
+	def __str__(self):
+		return str(self.title)
