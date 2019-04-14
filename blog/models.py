409833031256e8dd5cebec66e0363534d7928a91
Will Carhart <wcarhart@sandiego.edu>
2019-04-14 13:46:50 -0700

    Almost done with blog index

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 class Blog(models.Model):
 	article_number = models.IntegerField(default=0, blank=True, null=True)
@@ -6,6 +7,7 @@ class Blog(models.Model):
 	subtitle = models.CharField(max_length=100, default='', null=True, blank=True)
 	cover = models.FilePathField(path='/img', default='', null=True, blank=True)
 	content = models.TextField(default='', null=True, blank=True)
+	published = models.DateTimeField(default=datetime.datetime.now, null=True, blank=True)
 
 	def __str__(self):
 		return str(self.title)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Blog
 
 def blog_index(request):
-	return render(request, 'blog_index.html', {})
+	blog_entries = Blog.objects.all()
+	context = {
+		'blogs': blog_entries
+	}
+	return render(request, 'blog_index.html', context)
 
+def blog_detail(request, title):
+	blog_entry = Blog.objects.get(title=title)
+	context = {
+		'blog': blog_entry
+	}
+	return render(request, 'blog_detail.html', context)
