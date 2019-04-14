906134e8cad04755b2e334ad55328492f9261f7e
Will Carhart <wcarhart@sandiego.edu>
2019-04-13 18:39:53 -0700

    Fixed animations on project index

--- projects/templatetags/custom_filters.py
+++ projects/templatetags/custom_filters.py
@@ -11,4 +11,8 @@ def multiply(a, b):
 def resolve(_, name):
 	project = Project.objects.get(title=name)
 	return project.id
+
+@register.filter(name='mod')
+def mod(dividend, divisor):
+	return (dividend + 2) % divisor
 	
