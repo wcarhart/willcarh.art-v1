a1d00ae7ed1e7774b69daa02819b5a1ee98ab7ed
Will Carhart <wcarhart@sandiego.edu>
2019-04-15 04:55:03 -0700

    Refactored blog content structure

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-from bs4 import BeautifulSoup
 import json
 
 def blog_index(request):
@@ -14,13 +13,10 @@ def blog_detail(request, title):
 	post_title = title.replace('--', '%20').replace('-', ' ').replace('%20', ' ')
 	post = Post.objects.get(title=post_title)
 
-	sections = post.content.split('<section>')
 	cover_credit = json.loads(post.cover_credit)
-	# print(cover_credit)
 
 	context = {
 		'post': post,
-		'sections': sections,
 		'cover_credit': cover_credit
 	}
 
